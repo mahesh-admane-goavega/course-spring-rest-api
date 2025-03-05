5b5661abbb1cab7b4c5fb86b06cc627c3dcc4d42
Mahesh Admane <mahesh.admane@goavega.com>
2025-03-05 10:11:33 +0530

    updated annotation

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.springrest.springrest.entities.Course;
 import com.springrest.springrest.services.CourseService;
 
 @RestController
+@CrossOrigin(origins =  "*")
 public class MyController {
 
 	@Autowired
--- src/main/java/com/springrest/springrest/entities/Course.java
+++ src/main/java/com/springrest/springrest/entities/Course.java
@@ -1,12 +1,15 @@
 package com.springrest.springrest.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Course {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	private String title;
